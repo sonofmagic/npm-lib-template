50df9244fb7098dc24963de2c5a67e144a891152
SonOfMagic <qq1324318532@gmail.com>
2021-11-17 17:07:33 +0800

    feat: imporve jest and eslint config

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,12 @@
-import { foo } from '../src'
+import { foo, wait } from '../src'
 
 describe('[Default]', () => {
   test('foo should be bar', () => {
     expect(foo).toBe('bar')
+  })
+
+  test('wait 100ms', async () => {
+    const flag = await wait(100)
+    expect(flag).toBe(true)
   })
 })
