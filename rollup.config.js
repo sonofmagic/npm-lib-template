64859f9ce32e9d9cd8ae424699030cc69baca66c
ice breaker <1324318532@qq.com>
2021-12-27 18:01:13 +0800

    feat: add clean script and source map config

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,20 @@ import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import pkg from './package.json'
+// const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
 /** @type {import('rollup').RollupOptions} */
 const config = {
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: 'cjs',
+      sourcemap: isDev
       // exports: 'auto'
     },
-    { format: 'esm', file: pkg.module }
+    { format: 'esm', file: pkg.module, sourcemap: isDev }
   ],
 
   plugins: [
