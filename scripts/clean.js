f94e0f74dc985134dbdeecb10d04fb6442ea7501
SonOfMagic <1324318532@qq.com>
2022-03-17 19:12:48 +0800

    feat: commit new script and plugin

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,11 @@
-const del = require('del');
+const del = require('del')
 
-(async () => {
-  const deletedDirectoryPaths = await del(['dist'])
+// 同时兼容 `yarn clean` and `yarn clean xxx yyy zzz`
+;(async () => {
+  const dirs = process.argv.slice(2)
+  if (dirs.length === 0) {
+    dirs.push('dist')
+  }
+  const deletedDirectoryPaths = await del(dirs)
   console.log('Deleted directories:\n', deletedDirectoryPaths.join('\n'))
 })()
