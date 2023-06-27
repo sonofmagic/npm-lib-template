6232d37467476c2185c2167694d2b1b615099409
SonOfMagic <qq1324318532@gmail.com>
2023-06-27 17:47:39 +0800

    chore: add branches

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,13 @@
 /** @type {import('semantic-release').Options} */
 const options = {
-  branches: ['master', 'main']
+  branches: [
+    'master',
+    'main',
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ]
 }
 module.exports = options
