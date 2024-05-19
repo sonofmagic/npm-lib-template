3b3e55b20665117864beb07dc527028ba88ca8da
ice breaker <1324318532@qq.com>
2024-05-19 23:10:19 +0800

    chore: update release.config

--- release.config.cjs
+++ release.config.cjs
@@ -4,7 +4,6 @@ const options = {
     'master',
     'main',
     '+([0-9])?(.{+([0-9]),x}).x',
-    'master',
     'next',
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
