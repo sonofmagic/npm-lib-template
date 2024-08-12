f6b054e76c2250e0c1d522798dc3d24f136cc07a
ice breaker <1324318532@qq.com>
2024-08-12 10:55:05 +0800

    chore: add json

---
+++ demo/index.js
@@ -0,0 +1,3 @@
+import './index.json'
+
+console.log('--------------')
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,50 @@
-export const foo = 'bar'
+import { fileURLToPath } from 'node:url'
+import { watch } from 'rollup'
+import path from 'pathe'
 
-export function wait(timeout?: number) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true)
-    }, timeout)
-  })
-}
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-export const dirname = __dirname
+const watcher = watch({
 
-export * from '@/util'
+  // context: path.resolve(__dirname, '../demo'),
+  input: {
+    index: path.resolve(__dirname, '../demo/index.js'),
+  },
+  output: {
+    dir: path.resolve(__dirname, '../dist'),
+  },
+  plugins: [
+    {
+      name: 'xxx',
+      generateBundle() {
+        console.log(this.getWatchFiles())
+      },
+    },
+  ],
+  watch: {
+    include: ['./demo/**/*.json'],
+  },
+})
+
+watcher.on('event', (e) => {
+  if (e.code === 'START') {
+    console.log(e)
+  }
+  else if (e.code === 'BUNDLE_START') {
+    console.log(e)
+  }
+  else if (e.code === 'BUNDLE_END') {
+    console.log(e)
+  }
+  else if (e.code === 'END') {
+    console.log(e)
+  }
+  else if (e.code === 'ERROR') {
+    console.log(e)
+  }
+})
+
+watcher.on('change', (id, _change) => {
+  console.log(id)
+})
