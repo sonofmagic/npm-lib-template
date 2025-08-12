be0da7060685151910c34a338af82cdcc9094880
ice breaker <1324318532@qq.com>
2025-08-12 13:12:43 +0800

    fix: npm run script

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,7 +1,7 @@
 import process from 'node:process'
 import { deleteAsync } from 'del'
 
-// 同时兼容 `yarn clean` and `yarn clean xxx yyy zzz`
+// 同时兼容 `pnpm clean` and `pnpm clean xxx yyy zzz`
 const dirs = process.argv.slice(2)
 if (dirs.length === 0) {
   dirs.push('dist')
